a9a2cd131413e457d3913fd9f04d50e0bde70a2d
fsheahan <fred@primeacademy.io>
2016-08-27 21:48:59 -0500

    Unity Cookbook 1.8

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MenuActions : MonoBehaviour {
+	public void MENU_ACTION_GotoPage(string sceneName){
+		Application.LoadLevel(sceneName);
+	}
+}

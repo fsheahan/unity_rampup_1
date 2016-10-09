9a3437948047bbdcd2a930cd14b8d942937b425f
fsheahan <fred@primeacademy.io>
2016-10-08 20:57:32 -0500

    Fractal, instantiating objects

---
+++ Fractal/Assets/Fractal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+	
+	public Mesh mesh;
+	public Material material;
+
+	public int maxDepth;
+
+	private int depth;
+
+	private void Start () {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		if (depth < maxDepth) {
+			new GameObject("Fractal Child").
+				AddComponent<Fractal>().Initialize(this);
+		}
+	}
+
+	public float childScale;
+
+	private void Initialize (Fractal parent) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
+	}
+}
